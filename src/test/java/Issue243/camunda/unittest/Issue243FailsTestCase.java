d483fc38381871eecfa5522ee7659c30a5b81b44
nesies <>
2022-10-19 14:39:33 +0200

    init

---
+++ src/test/java/Issue243/camunda/unittest/Issue243FailsTestCase.java
@@ -0,0 +1,48 @@
+/*
+ * vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+ * -*- coding: utf-8 -*-
+ */
+package Issue243;
+
+import org.camunda.bpm.engine.*;
+import org.camunda.bpm.engine.ProcessEngine;
+
+import org.camunda.bpm.engine.exception.NotFoundException;
+import org.camunda.bpm.engine.task.Task;
+import org.camunda.bpm.engine.history.HistoricExternalTaskLog;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.complete;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.externalTask;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.withVariables;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.complete;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.processInstanceQuery;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.task;
+
+public class Issue243FailsTestCase {
+    @Rule
+    @ClassRule
+    public static ProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create().build();
+
+    @Test
+    @Deployment(resources = {
+        "Issue243/camunda/unittest/Issue243-fails.bpmn"
+        }
+    )
+    public void testFails() {
+        ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("Process_Issue243");
+
+        assertThat(processInstance).isActive();
+    }
+}
